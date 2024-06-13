79bc1e0aea1fb968034e091f83b1b6330e76b26f
[alt]legioner9 <legioner9@inbox.ru>
2024-06-13 19:35:01 +0700

    <> ##

--- .d/.ham/main/main.fn.lst.d/file_size_check_jpg.sh
+++ .d/.ham/main/main.fn.lst.d/file_size_check_jpg.sh
@@ -3,7 +3,7 @@
 echo -e "${BLUE}--- that file://${REPO_PATH}/${name_repo}/.d/.ham/main/main.fn.lst.d/file_size_check_jpg.sh ---${NORMAL}" #sistem info mesage
 echo -e "${HLIGHT}--- file_size_check_jpg_arg_mb_proc__ham__d() 50k ---${NORMAL}"                                         #start files
 
-if ! file_size_check_jpg_arg_mb_proc__ham__d "50"; then
+if ! file_size_check_jpg_arg_mb_proc__ham__d "100"; then
     plt_info "in file://${REPO_PATH}/${name_repo}/.d/.ham/main/main.fn.lst.d/file_size_check_jpg.sh : FAIL_EXEC : file_size_check_jpg_arg_mb_proc__ham__d() : return 1"
     return 1
 fi
