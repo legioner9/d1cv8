8cfe9f36a632f816b4180ed0d8befb32ead87abb
[alt]legioner9 <legioner9@inbox.ru>
2024-06-25 15:37:33 +0700

    <> ##

---
+++ .d/.arb/scr.arb/pr_1_mirr.ram/.grot/.data/__less/__part/.a/primum.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+path_file=${REPO_PATH}/d1cv8/.d/.arb/scr.arb/pr_1_mirr.ram/.grot/.data/__less/__part/.a/primum.sh
+# path_file=${REPO_PATH}/PATH_TO_THAT_FILE_FROM_REPO
+
+path_dir=$(dirname ${path_file})
